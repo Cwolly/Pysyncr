5ff6f94d72609bd0f4e6cc1cf20c98261f432694
chad <chad@cwdesktop.(none)>
2009-04-14 22:05:25 -0400

    changes to authentication to use paramiko library

--- authentication.py
+++ authentication.py
@@ -1,8 +1,12 @@
-''' Initial creation of a username and password. This eventually will need to be hashed somehow, and the password will need to be saved on the server somehow '''
+#!/usr/bin/env python
+""" Initial creation of a username and password. This eventually will need to be hashed somehow, and the password will need to be saved on the server somehow """
 
 import sys
+import paramiko #paramiko implements the python ssh library
 
-serverid = raw_input ("Please enter the IP or hostname of the pysyncr server: ") #needs to be stored for ssh and rsync connection later
+
+hostname = raw_input ("Please enter the IP or hostname of the pysyncr server: ") #needs to be stored for ssh and rsync connection later
+port = raw_input ("Please enter the port that the server uses to connect (default is 22):")
 username = raw_input ("Please create a username: ") #needs to be stored as permanent user authentication on server
 password = raw_input ("Please create a password: ") #needs to be stored as permanent user password on server
 
